f1c1a7d985b3c4f586c6407a9c1eebbb675e8f7e
Ibrokhimova Gulshanoy <ibragimovaguli62@gmail.com>
2022-12-04 15:49:47 +0500

    init: finished the project

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import HeaderImg from ";
+
+ function Header(){
+  
+    return (
+    <header className="header">
+       <img src="/images/header-img.jpg" className="header--img"  />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React course - Project3</h4>
+       
+    </header>
+    );
+}
+
+export default Header;
